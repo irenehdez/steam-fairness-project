b5d1a60c757390f9eec3b18d669aac393947bfd0
irenehdez <irene.hegui@gmail.com>
2025-12-06 16:20:22 +0100

    new version

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
+import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-DATA_DIR = BASE_DIR / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
+if "STEAM_DATA_DIR" in os.environ:
+    RAW_DATA_DIR = Path(os.environ["STEAM_DATA_DIR"])
+else:
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    DATA_DIR = BASE_DIR / "data"
+    RAW_DATA_DIR = DATA_DIR / "raw"
 
 INTERACTIONS_PATH = RAW_DATA_DIR / "train_interactions.csv"
 GAMES_PATH = RAW_DATA_DIR / "games.csv"
